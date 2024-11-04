4af2a8b340904e793f48e869e5beadd072617623
Gabriel Martin <gabrielmartin13009@gmail.com>
2024-11-04 14:12:14 +0100

    added carousel and cards and controllers and routes folders

--- database/db.js
+++ database/db.js
@@ -1,4 +1,5 @@
 const mariadb = require('mariadb');
+require('dotenv').config();
 
 const pool = mariadb.createPool({
     user: 'root',
--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express = require('express');
 const db = require('./database/db.js');
 const cors = require('cors');
 const app = express();
-const productRoute = require('./routes/productRoute');
-require('dotenv').config();
 const PORT = 3000;
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+const productRoute = require('./routes/productRoute');
+
 app.use('/products', productRoute);
+app.get('/products/search', productRoute.searchProducts);
+
 // Lancer le serveur
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
