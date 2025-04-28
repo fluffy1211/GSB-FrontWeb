955a702ea516015787b4b9739bf5b27822b7ec52
Gabriel Martin <gabrielmartin13009@gmail.com>
2025-04-28 09:31:57 +0200

    products pictures and db updated

--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointmentController.js');
-// Update import to use destructuring since auth now exports an object with multiple functions
 const { verifyToken } = require('../middleware/auth');
 
-// Now correctly use the verifyToken middleware
 router.post('/create', verifyToken, appointmentController.createAppointment);
 router.get('/list', verifyToken, appointmentController.getAppointments);
 router.delete('/:id', verifyToken, appointmentController.cancelAppointment);
